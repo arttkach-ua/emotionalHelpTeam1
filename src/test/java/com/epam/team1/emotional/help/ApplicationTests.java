3cae9499802d80f8b47c6241e86bc398d89de8ae
arttkach-ua <62747869+arttkach-ua@users.noreply.github.com>
2022-11-21 18:39:06 +0200

    initial commit

---
+++ src/test/java/com/epam/team1/emotional/help/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.team1.emotional.help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
