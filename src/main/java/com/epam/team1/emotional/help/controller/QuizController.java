cc86c6c67d3f3cca3fccf453532f6162ca0eb9e5
arttkach-ua <art.tkach@gmail.com>
2022-11-23 16:48:04 +0200

    fix

--- src/main/java/com/epam/team1/emotional/help/controller/QuizController.java
+++ src/main/java/com/epam/team1/emotional/help/controller/QuizController.java
@@ -3,6 +3,7 @@ package com.epam.team1.emotional.help.controller;
 import com.epam.team1.emotional.help.model.Quiz;
 import com.epam.team1.emotional.help.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class QuizController {
     QuizService quizService;
 
     @GetMapping
-    public List<Quiz> getQuizzes(){
-        return quizService.findAll();
+    public ResponseEntity<List<Quiz>> getQuizzes(){
+        return ResponseEntity.ok(quizService.findAll());
     }
 }
