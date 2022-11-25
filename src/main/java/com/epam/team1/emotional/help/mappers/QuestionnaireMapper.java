1dcf584ba9d92bb87e1c5393835c158d22d8eb16
Arthur_Khachatryan <arturakhachatryan1991@gmail.com>
2022-11-25 15:03:25 +0400

    complate DTO s

--- src/main/java/com/epam/team1/emotional/help/mappers/QuestionnaireMapper.java
+++ src/main/java/com/epam/team1/emotional/help/mappers/QuestionnaireMapper.java
@@ -1,12 +1,19 @@
 package com.epam.team1.emotional.help.mappers;
 
-import com.epam.team1.emotional.help.dto.QuestionResponseDTO;
 import com.epam.team1.emotional.help.dto.QuestionnaireResponseDTO;
 import com.epam.team1.emotional.help.model.Questionnaire;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
 public class QuestionnaireMapper {
+
+    private final QuestionMapper questionMapper;
+
+    public QuestionnaireMapper(QuestionMapper questionMapper) {
+        this.questionMapper = questionMapper;
+    }
 
     public QuestionnaireResponseDTO mapToResponseDto(Questionnaire questionnaire) {
 
@@ -14,7 +21,7 @@ public class QuestionnaireMapper {
         questionnaireResponseDTO.setId(questionnaire.getId());
         questionnaireResponseDTO.setName(questionnaire.getName());
         questionnaireResponseDTO.setDescription(questionnaire.getDescription());
-        //questionnaireResponseDTO.setQuestionResponseDTOList();
+        questionnaireResponseDTO.setQuestionResponseDTOList(questionnaire.getQuestionList().stream().map(questionMapper::mapToResponseDto).collect(Collectors.toList()));
         return questionnaireResponseDTO;
     }
 }
