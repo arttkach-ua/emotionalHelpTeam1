32c33e539d4317c643687afd3b679b845f7b946b
Arthur_Khachatryan <arturakhachatryan1991@gmail.com>
2022-11-26 21:24:50 +0400

    bug fixes

--- src/main/java/com/epam/team1/emotional/help/controller/QuestionnaireController.java
+++ src/main/java/com/epam/team1/emotional/help/controller/QuestionnaireController.java
@@ -2,18 +2,15 @@ package com.epam.team1.emotional.help.controller;
 
 import com.epam.team1.emotional.help.mappers.QuestionnaireMapper;
 import com.epam.team1.emotional.help.service.Impl.QuestionnaireService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/questionnaires")
 public class QuestionnaireController {
-    private QuestionnaireService questionnaireService;
-    private QuestionnaireMapper questionnaireMapper;
 
-    public QuestionnaireController(QuestionnaireService questionnaireService, QuestionnaireMapper questionnaireMapper) {
-        this.questionnaireService = questionnaireService;
-        this.questionnaireMapper = questionnaireMapper;
-    }
+    private final QuestionnaireService questionnaireService;
 
 }
