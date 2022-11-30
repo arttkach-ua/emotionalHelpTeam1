6405686799f6c1645452a1ada1dfd268e2a1cddf
arttkach-ua <art.tkach@gmail.com>
2022-11-30 17:35:38 +0200

    some refactoring Result creating finished

--- src/main/java/com/epam/team1/emotional/help/mappers/ResultResponseDtoMapper.java
+++ src/main/java/com/epam/team1/emotional/help/mappers/ResultResponseDtoMapper.java
@@ -1,26 +1,23 @@
 package com.epam.team1.emotional.help.mappers;
 
-import com.epam.team1.emotional.help.dto.ResultRequestDto;
+import com.epam.team1.emotional.help.dto.ResultResponseDto;
 import com.epam.team1.emotional.help.model.Result;
-import org.springframework.stereotype.Component;
 
-@Component
-public class ResultMapper {
-
-    public ResultRequestDto toDto(Result result){
-        return ResultRequestDto.builder()
+public class ResultResponseDtoMapper {
+    public ResultResponseDto toDto(Result result){
+        return ResultResponseDto.builder()
+                .id(result.getId())
                 .points(result.getPoints())
                 .questionnaireId(result.getQuestionnaire().getId())
                 .fullDescription(result.getFullDescription())
                 .shortDescription(result.getShortDescription())
                 .build();
     }
-    public Result fromDto(ResultRequestDto dto){
+    public Result fromDto(ResultResponseDto dto){
         return Result.builder()
                 .points(dto.getPoints())
                 .shortDescription(dto.getShortDescription())
                 .fullDescription(dto.getFullDescription())
                 .build();
     }
-
 }
--- src/main/java/com/epam/team1/emotional/help/service/QuizService.java
+++ src/main/java/com/epam/team1/emotional/help/service/QuizService.java
@@ -3,6 +3,7 @@ package com.epam.team1.emotional.help.service;
 import com.epam.team1.emotional.help.dto.QuizDto;
 import com.epam.team1.emotional.help.model.Answer;
 import com.epam.team1.emotional.help.model.Quiz;
+import com.epam.team1.emotional.help.model.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,17 @@ import org.springframework.stereotype.Service;
 public class QuizService {
     @Autowired
     private QuestionnaireService questionnaireService;
-
+    @Autowired
+    private ResultService resultService;
     @Autowired
     private AnswersService answersService;
 
-    public void calculateTest(QuizDto dto){
+    public void calculateQuiz(QuizDto dto){
         Quiz quiz = mapTestDtoToTest(dto);
         calculateTotalPoints(quiz);
         quiz.setTotalPoints(calculateTotalPoints(quiz));
+        Result result = resultService.getResult(quiz.getQuestionnaire(),quiz.getTotalPoints());
+        //Todo finish it
     }
 
     public Quiz mapTestDtoToTest(QuizDto dto){
