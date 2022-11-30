735705429305a1fdaf36da146b28be793e8e7b5e
arttkach-ua <art.tkach@gmail.com>
2022-11-30 13:24:35 +0200

    bug fixes + refactoring

--- src/main/java/com/epam/team1/emotional/help/mappers/QuestionMapper.java
+++ src/main/java/com/epam/team1/emotional/help/mappers/QuestionMapper.java
@@ -1,34 +1,34 @@
-//package com.epam.team1.emotional.help.mappers;
-//
-//import com.epam.team1.emotional.help.dto.AnswerResponseDTO;
-//import com.epam.team1.emotional.help.dto.QuestionResponseDTO;
-//import com.epam.team1.emotional.help.model.Answer;
-//import com.epam.team1.emotional.help.model.Question;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Component
-//public class QuestionMapper {
-//
-//    private final AnswerMapper answerMapper;
-//
-//    public QuestionMapper(AnswerMapper answerMapper) {
-//        this.answerMapper = answerMapper;
-//    }
-//
-//    public QuestionResponseDTO mapToResponseDto(Question question) {
-//        QuestionResponseDTO questionResponseDTO = new QuestionResponseDTO();
-//        questionResponseDTO.setId(question.getId());
-//        questionResponseDTO.setQuestion(question.getQuestion());
-//        questionResponseDTO.setAnswers(mapAll(question.getAnswersList()));
-//        return questionResponseDTO;
-//    }
-//
-//    private List<AnswerResponseDTO> mapAll(List<Answer> answers) {
-//        return answers.stream().map(answerMapper::mapToResponseDto).collect(Collectors.toList());
-//    }
-//
-//
-//}
+package com.epam.team1.emotional.help.mappers;
+
+import com.epam.team1.emotional.help.dto.AnswerResponseDTO;
+import com.epam.team1.emotional.help.dto.QuestionResponseDto;
+import com.epam.team1.emotional.help.model.Answer;
+import com.epam.team1.emotional.help.model.Question;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class QuestionMapper {
+
+    private final AnswerMapper answerMapper;
+
+    public QuestionMapper(AnswerMapper answerMapper) {
+        this.answerMapper = answerMapper;
+    }
+
+    public QuestionResponseDto mapToResponseDto(Question question) {
+        QuestionResponseDto questionResponseDTO = new QuestionResponseDto();
+        questionResponseDTO.setId(question.getId());
+        questionResponseDTO.setName(question.getName());
+        questionResponseDTO.setAnswers(mapAll(question.getAnswersList()));
+        return questionResponseDTO;
+    }
+
+    private List<AnswerResponseDTO> mapAll(List<Answer> answers) {
+        return answers.stream().map(answerMapper::mapToResponseDto).collect(Collectors.toList());
+    }
+
+
+}
--- src/main/java/com/epam/team1/emotional/help/model/BaseEntity.java
+++ src/main/java/com/epam/team1/emotional/help/model/BaseEntity.java
@@ -1,34 +1,25 @@
-//package com.epam.team1.emotional.help.model;
-//
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.MappedSuperclass;
-//import java.io.Serializable;
-//
-//@MappedSuperclass
-//@Setter
-//@Getter
-//@NoArgsConstructor
-//public class BaseEntity implements Serializable {
-//    private static final long serialVersionUID = 1L;
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    protected long id;
-//
-//    public BaseEntity(long id) {
-//        this.id = id;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//}
+package com.epam.team1.emotional.help.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+}
