2e0638ca79b1c2eaa3ac85314955920379f9f64b
arttkach-ua <art.tkach@gmail.com>
2022-12-01 18:47:29 +0200

    Revert "some refactoring"
    This reverts commit 6405686799f6c1645452a1ada1dfd268e2a1cddf.

---
+++ src/main/java/com/epam/team1/emotional/help/controller/QuestionnaireController.java
@@ -0,0 +1,16 @@
+package com.epam.team1.emotional.help.controller;
+
+import com.epam.team1.emotional.help.service.QuestionnaireService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/questionnaires")
+public class QuestionnaireController {
+
+    private final QuestionnaireService questionnaireService;
+
+
+}
