3279d69ef87e143fe2d7ad82b0878abfef7a6fba
Arthur_Khachatryan <arturakhachatryan1991@gmail.com>
2022-12-06 17:13:19 +0400

    bring developer changes to my branch

--- src/main/java/com/epam/team1/emotional/help/Application.java
+++ src/main/java/com/epam/team1/emotional/help/Application.java
@@ -15,57 +15,109 @@ import java.util.List;
 public class Application implements CommandLineRunner {
 
 
-	private final QuestionnaireRepository questionnaireRepository;
+    private final QuestionnaireRepository questionnaireRepository;
 
-	public Application(QuestionnaireRepository questionnaireRepository) {
-		this.questionnaireRepository = questionnaireRepository;
-	}
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public Application(QuestionnaireRepository questionnaireRepository) {
+        this.questionnaireRepository = questionnaireRepository;
+    }
 
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 
-//		if (!questionnaireRepository.existsByName("questionnaire 1")){
-//			Questionnaire questionnaire = new Questionnaire();
-//			questionnaire.setName("questionnaire 1");
-//			questionnaire.setDescription("questionnaire description");
-//
-//			List<Question> questionList = new ArrayList<>();
-//
-//			for (int i = 1; i < 4; i++) {
-//				Question question = new Question();
-//				question.setName("question " + i);
-//				question.setQuestionnaire(questionnaire);
-//				List<Answer> answerList = new ArrayList<>();
-//
-//				for (int j = 1; j < 4; j++) {
-//
-//					Answer answer = new Answer();
-//					answer.setName("answer " + j);
-//					answer.setPoints(j*j);
-//					answer.setQuestion(question);
-//
-//					answerList.add(answer);
-//					System.out.println(answer);
-//				}
-//				question.setAnswersList(answerList);
-//
-//				questionList.add(question);
-//			}
-//
-//			questionnaire.setQuestionList(questionList);
-//
-//			questionnaireRepository.save(questionnaire);
-//
-//
-//		}
+    @Override
+    public void run(String... args) throws Exception {
 
 
-	}
+        if (!questionnaireRepository.existsByName("questionnaire 1")) {
+
+
+            Questionnaire questionnaire = new Questionnaire();
+            questionnaire.setName("questionnaire 1");
+            questionnaire.setDescription("questionnaire description");
+
+            List<Question> questionList = new ArrayList<>();
+
+
+
+//            Question question1 = new Question();
+//            question1.setName("question 1");
+//            question1.setQuestionnaire(questionnaire);
+//
+//            Answer answer1q1 = new Answer();
+//            answer1q1.setQuestion(question1);
+//            answer1q1.setName("Answer 1q1");
+//            answer1q1.setPoints(2);
+//
+//            Answer answer2q1 = new Answer();
+//            answer2q1.setQuestion(question1);
+//            answer2q1.setName("Answer 2q1");
+//            answer2q1.setPoints(2);
+//
+//            Answer answer3q1 = new Answer();
+//            answer3q1.setQuestion(question1);
+//            answer3q1.setName("Answer 3q1");
+//            answer3q1.setPoints(2);
+//
+//            Answer answer4q1 = new Answer();
+//            answer1q1.setQuestion(question1);
+//            answer1q1.setName("Answer 4q1");
+//            answer1q1.setPoints(2);
+//
+//            ArrayList<Answer> answerListq1 = new ArrayList<>();
+//            answerListq1.add(answer1q1);
+//            answerListq1.add(answer2q1);
+//            answerListq1.add(answer3q1);
+//            answerListq1.add(answer4q1);
+//
+//            question1.setAnswersList(answerListq1);
+
+            Question question2 = new Question();
+            question2.setName("question 2");
+            question2.setQuestionnaire(questionnaire);
+
+            Answer answer1q2 = new Answer();
+            answer1q2.setQuestion(question2);
+            answer1q2.setName("Answer 1q2");
+            answer1q2.setPoints(2);
+
+            Answer answer2q2 = new Answer();
+            answer2q2.setQuestion(question2);
+            answer2q2.setName("Answer 2q2");
+            answer2q2.setPoints(2);
+
+            Answer answer3q2 = new Answer();
+            answer3q2.setQuestion(question2);
+            answer3q2.setName("Answer 3q2");
+            answer3q2.setPoints(2);
+
+            Answer answer4q2 = new Answer();
+            answer4q2.setQuestion(question2);
+            answer4q2.setName("Answer 4q2");
+            answer4q2.setPoints(2);
+
+
+            ArrayList<Answer> answerListq2 = new ArrayList<>();
+            answerListq2.add(answer1q2);
+            answerListq2.add(answer2q2);
+            answerListq2.add(answer3q2);
+            answerListq2.add(answer4q2);
+
+            question2.setAnswersList(answerListq2);
+
+        //    questionList.add(question1);
+            questionList.add(question2);
+
+
+        questionnaire.setQuestionList(questionList);
+
+        questionnaireRepository.save(questionnaire);
+
+
+    }
+
+
+}
 }
