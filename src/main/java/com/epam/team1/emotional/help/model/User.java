9112df997c5f85a7123a6d01638aae830b573a54
Arthur_Khachatryan <arturakhachatryan1991@gmail.com>
2022-12-07 22:42:33 +0400

    add getCurrentUser  functionality

--- src/main/java/com/epam/team1/emotional/help/model/User.java
+++ src/main/java/com/epam/team1/emotional/help/model/User.java
@@ -3,10 +3,7 @@ package com.epam.team1.emotional.help.model;
 
 import com.epam.team1.emotional.help.model.enums.Gender;
 import com.epam.team1.emotional.help.model.enums.Role;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -17,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class User extends BaseEntity {
--- src/main/java/com/epam/team1/emotional/help/service/UserService.java
+++ src/main/java/com/epam/team1/emotional/help/service/UserService.java
@@ -1,11 +1,23 @@
 package com.epam.team1.emotional.help.service;
 
+import com.epam.team1.emotional.help.model.User;
+import com.epam.team1.emotional.help.security.UserDetailsImplementation;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    public Boolean userIsAuthorized(){
+    public Boolean userIsAuthorized() {
         //TODO finish it
         return Boolean.TRUE;
+    }
+
+
+
+    private User getCurrentUser() {
+        return ((UserDetailsImplementation) SecurityContextHolder.
+                getContext().
+                getAuthentication().
+                getPrincipal()).getUser();
     }
 }
